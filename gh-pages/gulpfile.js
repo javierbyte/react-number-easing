3ac4281799afac62ba3dc4b0fcb6ba570be5d848
Josh <1719239+che-wf@users.noreply.github.com>
2020-07-21 10:59:31 -0600

    Update dependencies

--- gh-pages/gulpfile.js
+++ gh-pages/gulpfile.js
@@ -11,27 +11,29 @@ const watchify = require('watchify');
 
 // add custom browserify options here
 const customOpts = {
-	entries: ['./src/js/app.jsx'],
-	debug: true,
+  entries: ['./src/js/app.jsx'],
+  debug: true,
 };
 const opts = assign({}, watchify.args, customOpts);
-const b = watchify(browserify(opts)
-	.transform(babelify));
+const b = watchify(browserify(opts).transform(babelify));
 
 gulp.task('js', bundle); // so you can run `gulp js` to build the file
 
-gulp.task('less', () => gulp.src('./src/styles/main.less')
-	.pipe(less())
-	.on('error', function (err) {
-		console.log('LESS ERROR', err);
-		this.emit('end');
-	})
-	.pipe(gulp.dest('./dist'))
-	.pipe(livereload()));
+gulp.task('less', () =>
+  gulp
+    .src('./src/styles/main.less')
+    .pipe(less())
+    .on('error', function (err) {
+      console.log('LESS ERROR', err);
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./dist'))
+    .pipe(livereload())
+);
 
 gulp.task('lessWatch', () => {
-	livereload.listen();
-	gulp.watch('src/styles/**/*.less', ['less']);
+  livereload.listen();
+  gulp.watch('src/styles/**/*.less', ['less']);
 });
 
 gulp.task('default', ['js', 'lessWatch']);
@@ -40,33 +42,36 @@ b.on('update', bundle); // on any dep update, runs the bundler
 b.on('log', gutil.log); // output build logs to terminal
 
 function bundle() {
-	return b.bundle()
-		// log errors if they happen
-		.on('error', function (err) {
-			console.log('\n');
-			gutil.log(gutil.colors.yellow('BROWSERIFY ERROR'));
+  return (
+    b
+      .bundle()
+      // log errors if they happen
+      .on('error', function (err) {
+        console.log('\n');
+        gutil.log(gutil.colors.yellow('BROWSERIFY ERROR'));
 
-			if (err.filename) {
-				const path = err.filename.split('\\');
-				let errLoc = '';
-				if ('loc' in err) {
-					errLoc = ` (${err.loc.line}:${err.loc.column})`;
-				}
+        if (err.filename) {
+          const path = err.filename.split('\\');
+          let errLoc = '';
+          if ('loc' in err) {
+            errLoc = ` (${err.loc.line}:${err.loc.column})`;
+          }
 
-				path.splice(0, 3);
+          path.splice(0, 3);
 
-				gutil.log(gutil.colors.yellow('File:', path.join('/') + errLoc));
-				if ('codeFrame' in err) {
-					gutil.log(gutil.colors.yellow('Error:\n', err.codeFrame));
-				}
-				this.emit('end');
-			} else {
-				gutil.log(err);
-			}
-		})
+          gutil.log(gutil.colors.yellow('File:', path.join('/') + errLoc));
+          if ('codeFrame' in err) {
+            gutil.log(gutil.colors.yellow('Error:\n', err.codeFrame));
+          }
+          this.emit('end');
+        } else {
+          gutil.log(err);
+        }
+      })
 
-		.pipe(source('app.js'))
+      .pipe(source('app.js'))
 
-		.pipe(gulp.dest('./dist'))
-		.pipe(livereload());
+      .pipe(gulp.dest('./dist'))
+      .pipe(livereload())
+  );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -9,136 +9,142 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 class NumberEasing extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		const { value, precision, trail } = this.props;
+    const { value, precision, trail } = this.props;
 
-		this.timeout = null;
-		this.startAnimationTime = null;
-		this.state = {
-			displayValue: value,
-			// eslint-disable-next-line no-restricted-globals
-			previousValue: trail && !isNaN(parseFloat(value)) ?
-				parseFloat(value).toFixed(precision > -1 ? precision : 0) : value,
-		};
-	}
+    this.timeout = null;
+    this.startAnimationTime = null;
+    this.state = {
+      displayValue: value,
+      // eslint-disable-next-line no-restricted-globals
+      previousValue:
+        trail && !isNaN(parseFloat(value))
+          ? parseFloat(value).toFixed(precision > -1 ? precision : 0)
+          : value,
+    };
+  }
 
-	componentWillReceiveProps(nextProps) {
-		const { value } = this.props;
+  componentWillReceiveProps(nextProps) {
+    const { value, delayValue } = this.props;
+    const { displayValue } = this.state;
 
-		if (parseFloat(nextProps.value) === value) {
-			return;
-		}
+    if (parseFloat(nextProps.value) === value) {
+      return;
+    }
 
-		this.setState({
-			previousValue: this.state.displayValue,
-		});
+    this.setState({
+      previousValue: displayValue,
+    });
 
-		if (!window.isNaN(parseInt(this.props.delayValue, 10))) {
-			this.delayTimeout = setTimeout(() => {
-				this.startAnimationTime = new Date().getTime();
-				this.updateNumber();
-			}, this.props.delayValue);
-		} else {
-			this.startAnimationTime = new Date().getTime();
-			this.updateNumber();
-		}
-	}
+    if (!window.isNaN(parseInt(delayValue, 10))) {
+      this.delayTimeout = setTimeout(() => {
+        this.startAnimationTime = new Date().getTime();
+        this.updateNumber();
+      }, delayValue);
+    } else {
+      this.startAnimationTime = new Date().getTime();
+      this.updateNumber();
+    }
+  }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		return nextState.displayValue !== this.state.displayValue;
-	}
+  shouldComponentUpdate(nextProps, nextState) {
+    const { displayValue } = this.state;
+    return nextState.displayValue !== displayValue;
+  }
 
-	updateNumber = () => {
-		const { value, precision } = this.props;
-		const now = new Date().getTime();
-		const elapsedTime = Math.min(
-			this.props.speed,
-			now - this.startAnimationTime,
-		);
-		const progress = eases[this.props.ease](elapsedTime / this.props.speed);
+  updateNumber = () => {
+    const { value, precision, speed, ease } = this.props;
+    const { previousValue } = this.state;
+    const now = new Date().getTime();
+    const elapsedTime = Math.min(speed, now - this.startAnimationTime);
+    const progress = eases[ease](elapsedTime / speed);
 
-		const currentDisplayValue = Math.round(((
-			(value - this.state.previousValue) * progress) + this.state.previousValue) *
-			Math.pow(10, precision)) / Math.pow(10, precision);
+    const currentDisplayValue =
+      Math.round(
+        ((value - previousValue) * progress + previousValue) *
+          Math.pow(10, precision)
+      ) / Math.pow(10, precision);
 
-		this.setState({
-			displayValue: currentDisplayValue,
-		});
+    this.setState({
+      displayValue: currentDisplayValue,
+    });
 
-		if (elapsedTime < this.props.speed) {
-			this.timeout = setTimeout(this.updateNumber, 16);
-		} else {
-			this.setState({
-				previousValue: value,
-			});
-		}
-	}
+    if (elapsedTime < speed) {
+      this.timeout = setTimeout(this.updateNumber, 16);
+    } else {
+      this.setState({
+        previousValue: value,
+      });
+    }
+  };
 
-	componentWillUnmount() {
-		clearTimeout(this.timeout);
-		clearTimeout(this.delayTimeout);
-	}
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+    clearTimeout(this.delayTimeout);
+  }
 
-	render() {
-		const {
-			className,
-			currency,
-			delayValue,
-			ease,
-			locale,
-			precision,
-			speed,
-			trail,
-			useLocaleString,
-			value,
-			...other
-		} = this.props;
-		const { displayValue } = this.state;
+  render() {
+    const {
+      className,
+      currency,
+      delayValue,
+      ease,
+      locale,
+      precision,
+      speed,
+      trail,
+      useLocaleString,
+      value,
+      ...other
+    } = this.props;
+    const { displayValue } = this.state;
 
-		let classes = 'react-number-easing';
-		if (className) {
-			classes += ` ${className}`;
-		}
+    let classes = 'react-number-easing';
+    if (className) {
+      classes += ` ${className}`;
+    }
 
-		const opts = {};
-		if (useLocaleString && currency) {
-			opts.currency = currency;
-			opts.style = 'currency';
-		}
+    const opts = {};
+    if (useLocaleString && currency) {
+      opts.currency = currency;
+      opts.style = 'currency';
+    }
 
-		return (
-			<span {...other} className={classes}>
-				{useLocaleString ? parseFloat(displayValue).toLocaleString(locale, opts) :
-					trail ? parseFloat(displayValue).toFixed(precision > -1 ? precision : 0) : displayValue
-				}
-			</span>
-		);
-	}
+    return (
+      <span {...other} className={classes}>
+        {useLocaleString
+          ? parseFloat(displayValue).toLocaleString(locale, opts)
+          : trail
+          ? parseFloat(displayValue).toFixed(precision > -1 ? precision : 0)
+          : displayValue}
+      </span>
+    );
+  }
 }
 
 NumberEasing.propTypes = {
-	currency: PropTypes.string,
-	delayValue: PropTypes.number,
-	ease: PropTypes.oneOf(Object.keys(eases)),
-	locale: PropTypes.string,
-	precision: PropTypes.number,
-	speed: PropTypes.number,
-	trail: PropTypes.bool,
-	useLocaleString: PropTypes.bool,
-	value: PropTypes.any.isRequired,
+  currency: PropTypes.string,
+  delayValue: PropTypes.number,
+  ease: PropTypes.oneOf(Object.keys(eases)),
+  locale: PropTypes.string,
+  precision: PropTypes.number,
+  speed: PropTypes.number,
+  trail: PropTypes.bool,
+  useLocaleString: PropTypes.bool,
+  value: PropTypes.any.isRequired,
 };
 
 NumberEasing.defaultProps = {
-	currency: '',
-	delayValue: 50,
-	ease: 'quintInOut',
-	locale: 'en-US',
-	precision: 2,
-	speed: 500,
-	trail: false,
-	useLocaleString: true,
+  currency: '',
+  delayValue: 50,
+  ease: 'quintInOut',
+  locale: 'en-US',
+  precision: 2,
+  speed: 500,
+  trail: false,
+  useLocaleString: true,
 };
 
 export default NumberEasing;
